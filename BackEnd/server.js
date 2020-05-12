267971fb7e926e84de5f82903086a598b898901c
Jose Carlos Jimenez <55406693+Jose-Carlos-Jimenez@users.noreply.github.com>
2020-05-12 01:40:17 -0600

    [UPDATE] Estableciendo interface entre Front End y Back End

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,28 +1,61 @@
+/**
+ * Le decimos a la aplicación que librerias vamos a utilizar.
+*/
 var fs = require('fs'); 
 var parser = require('./gramatica');
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
 
-app.get('/parse', (req, res ) =>
-{
-    var analyze = req.body;
-    console.log(analyze);
-    res.send('Archivo de entrada recibido.'); 
-});
+/**
+ * Sirve para poder recibir los archivos en formato JSON.
+*/
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
+/**
+ * Le damos permisos para acceder.
+*/
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
 
 app.listen(3000, () => console.log('App running on port 3000.'));
 
-var ast;
-try {
-    // leemos nuestro archivo de entrada
-    var entrada = fs.readFileSync('./entrada.txt');
-    // invocamos a nuestro parser con el contendio del archivo de entradas
-    ast = parser.parse(entrada.toString());
 
-    // imrimimos en un archivo el contendio del AST en formato JSON
-    fs.writeFileSync('./ast.json', JSON.stringify(ast, null, 2));
-} catch (e) {
-    console.error(e);
-    return;
+app.post('/parse', (req, res) =>
+{
+    console.log("\n<----------------- INICIO DE ANÁLISIS ------------------------->");
+    var analyze = req.body.DATA;
+    //console.log(analyze);
+    getAst(analyze.toString());
+    console.log("\n<------------------- FIN DE ANÁLISIS -------------------------->");
+    res.send();    
+});
+
+app.get('/', (req, res) => 
+{
+    console.log("Activo");
+    res.send("Sevidor escuchando");
+});
+
+
+function getAst(texto)
+{
+    var ast;
+    try {
+        // invocamos a nuestro parser con el contendio del archivo de entradas
+        ast = parser.parse(texto);
+    
+        // imrimimos en un archivo el contendio del AST en formato JSON
+        // fs.writeFileSync('./ast.json', JSON.stringify(ast, null, 2));
+    } catch (e) {
+        console.error(e);
+        return;
+    }
 }
--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -11,3 +11,47 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 }
+
+function getText()
+{
+  var editor = ace.edit("editor");
+  var contenido = editor.getSession().getValue();
+  var data = {DATA : contenido.toString()};
+
+  fetch('http://localhost:3000/parse', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    
+  })
+    .then((response) => 
+    {
+      console.log("Recibido.");
+    })
+    .catch((error) => console.error("Error: ", error));
+}
+
+function openFile(e)
+{
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    // Display file content
+    console.log(contents);
+    displayContents(contents);
+  };
+  reader.readAsText(file);
+}
+
+function displayContents(contents) {
+  var editor = ace.edit("editor");
+  editor.getSession().setValue(contents);
+}
+
+document.getElementById('file-input').addEventListener('change', openFile, false);
